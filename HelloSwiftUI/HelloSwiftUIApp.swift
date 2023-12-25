712042d3e7f2776c9d4c12304ce0dc6769279b5a
Mustafa Cakir <98mustafacan@gmail.com>
2023-12-25 21:30:10 +0300

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by MustafaCan on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
